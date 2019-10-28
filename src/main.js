140578c076f7dc264a4c3cf8baec7ca63b54aaa1
ColeyG <cjgeerts@gmail.com>
2019-10-28 17:33:34 -0400

    Initial Styles

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import '../styles/reset.css';
 import '../styles/main.scss';
 import 'babel-polyfill';
+import 'promise-polyfill/src/polyfill';
+import 'whatwg-fetch';
+
+const respDiv = document.querySelector('.response');
+
+async function fetchData() {
+  await fetch('http://localhost:3000', { method: 'GET', mode: 'cors' })
+    .then((resp) => resp.text())
+    .then((data) => {
+      console.log(data);
+      respDiv.appendChild(document.createTextNode(data));
+    });
+}
+
+const initialize = () => {
+  fetchData();
+};
+
+// initialize();
